aea5e33f30109c0979bdf18a40fc4b61f83cf52a
Yulakheylo <yulakheylo@gmail.com>
2024-02-03 10:06:22 +0300

    poisk

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import sys
-from geocoder import get_coordinates, get_ll_span
+from business import find_business
+from geocoder import get_coordinates
+from distance import lonlat_distance
 from mapapi import show_map
 
 toponym_to_find = " ".join(sys.argv[1:])
-if toponym_to_find:
-    lat, lon = get_coordinates(toponym_to_find)
-    ll_spn = f"ll={lat},{lon}&spn=0.005,0.005"
-    show_map(ll_spn, "map")
 
-    ll, spn = get_ll_span(toponym_to_find)
-    ll_spn = f"ll={ll}&spn={spn}"
-    show_map(ll_spn, "map")
+lat, lon = get_coordinates(toponym_to_find)
+address_ll = f"{lat},{lon}"
+span = "0.005,0.005"
 
-    point_param = f"pt={ll}"
-    show_map(ll_spn, "map", add_params=point_param)
-else:
-    print('No data')
+organization = find_business(address_ll, span, "аптека")
+point = organization["geometry"]["coordinates"]
+org_lat, org_lon = float(point[0]), float(point[1])
+
+points_param = f"pt={org_lat},{org_lon},pm2dgl~{address_ll},pm2rdl"
+show_map(f"ll={address_ll}&spn={span}", "map", add_params=points_param)
+
+show_map(map_type="map", add_params=points_param)
+
+try:
+    name = organization["properties"]["CompanyMetaData"]["name"]
+    address = organization["properties"]["CompanyMetaData"]["address"]
+    time = organization["properties"]["CompanyMetaData"]["Hours"]["text"]
+    distance = round(lonlat_distance((lon, lat), (org_lon, org_lat)))
+    snippet = f"Название:\t{name}\nАдрес:\t{address}\nВремя работы:\t{time}\nРасстояние:\t{distance}м."
+    print(snippet)
+except KeyError:
+    print("Ошибка. Не указаны данные.")
